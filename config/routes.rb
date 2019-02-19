26e02ff82a83570592e5ede6ad306de3142d7c5a
YumeiLi <yumeili@tamu.edu>
2019-02-19 00:10:38 -0600

    Basic layout of login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  resources :home
+  resources :login
+
+  get 'logout', to: 'login#logout'
+
 end
