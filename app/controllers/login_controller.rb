7e6096479bbed93d43a1641bb4aca68700d906f3
YumeiLi <yumeili@tamu.edu>
2019-02-19 14:44:25 -0600

    with controller

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,20 @@
+class LoginController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def create
+    if not params.has_key?('username') or params['username'].empty?
+      redirect_to login_index_path
+    elsif not params.has_key?('password') or params['password'].empty?
+      redirect_to login_index_path
+    else
+      session['login'] = params['username']
+    end
+  end
+
+  def logout
+    session['login'] = nil
+    redirect_to home_path(:index)
+  end
+end
