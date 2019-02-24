94068a51091c7d4b5a7e54dd4e7098ed5ec8c7bc
YumeiLi <yumeili@tamu.edu>
2019-02-24 17:29:16 -0600

    Linked all pages

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,7 +7,8 @@ class LoginController < ApplicationController
     account = Account.find_by_username(params[:username])
     if account && account.authenticate(params[:password])
       session[:account_id] = account.id
-      redirect_to root_url, notice: "Logged in!"
+      session['login'] = account.username
+      redirect_to rides_path, notice: "Logged in!"
     else
       flash.now[:alert] = "Email or password is invalid"
       render "index"
