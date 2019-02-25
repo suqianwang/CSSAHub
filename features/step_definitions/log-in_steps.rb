cee6bb22edf3d3ee21380a0d8ffd892065223ce8
YumeiLi <yumeili@tamu.edu>
2019-02-25 16:04:51 -0600

    merged testing branch, delete comment in bdd

--- features/step_definitions/log-in_steps.rb
+++ features/step_definitions/log-in_steps.rb
@@ -1,18 +1,13 @@
 Given("I am in the log in page") do
   require 'factory_girl_rails'
   @account = FactoryGirl.create(:account)
-  Account.all.each do |user|
-    puts user.username
-  end
   visit login_index_path
 end
 
 When("I fill in my username, password and press Login button") do
 
   fill_in "Username", :with => @account.username
-  puts @account.username
   fill_in "Password", :with => @account.password
-  puts @account.password
   click_button 'Login'
 
 end
