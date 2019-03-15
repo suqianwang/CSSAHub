40f175715c6c9b270187ad02c01f585255c72cee
tamuaschaef1210 <tamuaschaef1210@tamu.edu>
2019-03-15 13:35:28 -0500

    admin page BDD tests complete

---
+++ features/step_definitions/admin-page_steps.rb
@@ -0,0 +1,34 @@
+Given("I am an admin") do
+  require 'factory_bot_rails'
+  @account = FactoryBot.create :account, :admin
+  visit login_index_path
+  fill_in "Username", :with => @account.username
+  fill_in "Password", :with => @account.password
+  click_button 'Login'
+end
+
+Given("{string} exists on the admin page") do |string|
+  @option = string
+end
+
+When("I view the admin page") do
+  visit admin_index_path
+end
+
+Then("I should see {string} on the admin page") do |string|
+  expect(page).to have_content('Admin Page')
+end
+
+Given("I want to go to the {string} page from the admin page") do |string|
+  @option = string
+  visit admin_index_path
+end
+
+When("I select the option on the admin page") do
+  click_link @option
+  visit accounts_path
+end
+
+Then("the site should navigate to the {string} page from the admin page") do |string|
+  expect(page).to have_content('Accounts')
+end
