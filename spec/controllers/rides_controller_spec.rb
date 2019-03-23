ccba62edfc803df23df5f42cbf2640e471ba4da6
David Ratliff <ratlidav@tamu.edu>
2019-03-23 17:52:44 +0000

    added TDD test cases

--- spec/controllers/rides_controller_spec.rb
+++ spec/controllers/rides_controller_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe RidesController, :type => :controller do
-before do
-    @account = FactoryBot.create :account, :user
+  before do
+    @ride = FactoryBot.create :ride
   end
 
   describe "GET index" do
     it "assigns @rides as all rides" do
-	  login(@account)
-      ride = Ride.create!(role: "passenger")
+	  login(@ride.account)
+      ride = Ride.create!(@ride.params)
       get :index
       expect(assigns(:rides)).to eq([ride])
     end
@@ -20,22 +20,21 @@ before do
     end
   end
       
-  
   describe "POST create" do
     it "saves a new ride" do
       assert_difference 'Ride.count' do
-        post :create, :params => { :ride => { :role => 'passenger' } }
+        post :create, :params => @ride.params
       end
     end
     
     it "redirects to the index" do
-      post :create, :params => { :ride => { :role => 'driver' } }
+      post :create, :params => @ride.params
       expect(response).to redirect_to(driver_path)
     end
     
     context "with valid attributes" do
       it "assigns and saves created ride as @ride" do
-        post :create, :params => { :ride => { :role => 'passenger' } }
+        post :create, :params => @ride.params
         expect(assigns(:ride)).to be_a(Ride)
         expect(assigns(:ride)).to be_persisted
       end
@@ -44,9 +43,18 @@ before do
     context "with invalid attributes" do
       it "assigns created ride as @ride" do
         Ride.any_instance.stub(:save).and_return(false)
-        post :create, :params => { :ride => { :role => 'invalid role' } }
+        @ride.role = 'invalid role'
+        post :create, :params => @ride.params
         expect(assigns(:ride)).to be_a(Ride)
       end
+    end
+  end
+  
+  describe "GET show" do
+    it "assigns the requested ride as @ride" do
+      ride = Ride.create!(@ride)
+      get :show, {:id => ride.to_param}
+      expect(assigns(:ride)).to_eq ride
     end
   end
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,8 +1,66 @@
 RSpec.describe Ride, :type => :model do
+  before do 
+    @ride = FactoryBot.create :ride
+  end
+  
+  describe '#account' do
+    it { is_expected.to validate_associated :account }
+  end
+  
   describe '#role' do
     it { is_expected.to validate_presence_of(:role) }
     it { is_expected.to allow_value("passenger").for(:role) }
     it { is_expected.to_not allow_value("invalid role").for(:role) }
   end
-      
+  
+  describe '#locations' do
+    it { is_expected.to validate_presence_of(:departure, :destination) }
+    it { is_expected.to allow_value("Zachry").for(:departure, :destination) }
+    it { is_expected.to_not allow_value("invalid location").for(:departure, :destination) }
+  end
+  
+  describe '#dates' do
+    it { is_expected.to validate_presence_of(:start_date, :end_date) }
+  end
+  
+  describe '#date_order' do
+    it 'validates end date is after begin date' do
+      context 'with valid attributes' do
+        @ride.start_date = Date.today+1
+        @ride.end_date = Date.today+2
+        expect(@ride).to be_valid
+      end
+      context 'with invalid attributes' do
+        @ride.start_date = Date.today+2
+        @ride.end_date = Date.today+1
+      end
+    end
+    it 'validates that start_date is not today' do
+      @ride.start_date = Date.today
+      @ride.end_date = Date.today+1
+    end
+  end
+  
+  describe '#time' do
+    it { is_expected.to validate_presence_of(:start_time, :end_time) }
+  end
+  
+  describe '#time_order' do
+    it 'validates end time is after begin time' do
+      context 'with valid attributes' do
+        @ride.start_time = Time.current
+        @ride.end_time = Time.current+2
+        expect(@ride).to be_valid
+      end
+      context 'with invalid attributes' do
+        @ride.start_time = Time.current+2
+        @ride.end_time = Time.current+1
+      end
+    end
+  end
+  
+  describe '#seats' do
+    it { is_expected.to validate_presence_of(:seats) }
+    it { is_expected.to validate_inclusion_of :seats, in: 1..8 }
+  end
 end
