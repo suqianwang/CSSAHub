55987223c1b216667eec4e03ba4e21eb6ae91230
David Ratliff <ratlidav@tamu.edu>
2019-03-25 03:42:25 +0000

    added rides show page, prettified rides index page, fixed bug in determining current user when session remembers account_id but database is empty

--- db/schema.rb
+++ db/schema.rb
@@ -38,11 +38,4 @@ ActiveRecord::Schema.define(version: 2019_03_23_183713) do
     t.index ["account_id"], name: "index_rides_on_account_id"
   end
 
-  create_table "services", force: :cascade do |t|
-    t.string "type"
-    t.string "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
