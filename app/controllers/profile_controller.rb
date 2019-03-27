b0e2b1f14f1d7a5d56a1d9a2227fdd70c6c9e39c
megan-grahmann <megan.grahmann@tamu.edu>
2019-03-27 17:16:05 -0500

    added username for profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,7 +1,25 @@
 class ProfileController < ApplicationController
 before_action :login_required, :only => :index
 
-  def index
-  end
 
+  def index
+    @profile = Account.find_by_username(session[:login])
+	puts @profile
+
+  end
+  
+  def edit
+        # use this to populate a form in your view
+  end
+	
+	
+  private
+    def profile_params
+      params.require(:account).permit(:username, :email, :name, :password, :password_confirmation, :terms_and_conditions)
+    end
+
+
+    def set_profile
+      @profile = Account.find(params[:id])
+	end
 end
