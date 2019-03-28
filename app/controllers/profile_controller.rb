9988681c735fee928c635ecf9b96346f29eac294
megan-grahmann <megan.grahmann@tamu.edu>
2019-03-27 19:12:41 -0500

    where username display rides

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,7 +4,8 @@ before_action :login_required, :only => :index
 
   def index
     @profile = Account.find_by_username(session[:login])
-	puts @profile
+	@posts = Ride.where(:role => "driver", :account_id => session[:account_id])
+	@requests = Ride.where(:role => "passenger", :account_id => session[:account_id])
 
   end
   
