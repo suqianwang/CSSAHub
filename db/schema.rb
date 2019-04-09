641f9058d65c498cb8e478b37e813d6ee9d1e83f
YumeiLi <yumeili@tamu.edu>
2019-04-09 07:35:08 -0500

    added start_datetime and end_datetime columns to rides, fixed notice on ride creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_222022) do
+ActiveRecord::Schema.define(version: 2019_04_09_120121) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "username"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_04_08_222022) do
     t.float "departure_lon"
     t.float "destination_lat"
     t.float "destination_lon"
+    t.datetime "start_datetime"
+    t.datetime "end_datetime"
     t.index ["account_id"], name: "index_rides_on_account_id"
   end
 
