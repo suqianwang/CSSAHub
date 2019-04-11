c6bf04630ffed026449451ff4a68237d44d2db97
Suqian Wang <wangsuqian123@tamu.edu>
2019-04-11 11:05:34 -0500

    BDD coverage 100%

--- features/step_definitions/create-ride_steps.rb
+++ features/step_definitions/create-ride_steps.rb
@@ -10,6 +10,19 @@ Given("I am logged in") do
   end
 end
 
+Given("A matching ride exists") do
+  @matching_ride = FactoryBot.create(:ride)
+end
+
+Given("A passenger ride exists that is not a match because of capacity") do
+  @p_matching_ride = FactoryBot.create(:ride, :seats => 5, :role =>'passenger')
+end
+
+
+Given("A driver ride exists that is not a match because of capacity") do
+  @d_matching_ride = FactoryBot.create(:ride, :seats => 1, :role => 'driver')
+end
+
 Given("I am on the new ride page") do
   visit new_ride_path
 end
@@ -20,7 +33,7 @@ When("I select my role as {string}") do |string|
 end
 
 When("I fill in the form") do
-  @ride_params = { departure: "Zachry Engineering Center, Spence Street, College Station, TX, USA", destination: "George Bush Intercontinental Airport (IAH), North Terminal Road, Houston, TX, USA", start_date: (Date.today).strftime("%m/%d/%Y"), end_date: (Date.today+1).strftime("%m/%d/%Y"), start_time: "8:00", end_time: "12:00", seats: 2 }
+  @ride_params = { departure: "Zachry Engineering Center, Spence Street, College Station, TX, USA", destination: "George Bush Intercontinental Airport (IAH), North Terminal Road, Houston, TX, USA", start_date: (Date.today).strftime("%m/%d/%Y"), start_time: "08:00", end_time: "12:00", seats: 2 }
   fill_in('ride_departure', with: @ride_params[:departure])
   fill_in('ride_destination', with: @ride_params[:destination])
   fill_in('ride_start_date', with: @ride_params[:start_date])
@@ -28,6 +41,17 @@ When("I fill in the form") do
   fill_in('ride_end_time', with: @ride_params[:end_time])
   fill_in('ride_seats', with: @ride_params[:seats])
 end
+
+When("I fill in the form with a matching ride") do
+  # @ride_params = { departure: "Zachry Engineering Center, Spence Street, College Station, TX, USA", destination: "George Bush Intercontinental Airport (IAH), North Terminal Road, Houston, TX, USA", start_date: (Date.today).strftime("%m/%d/%Y"), start_time: "08:00", end_time: "12:00", seats: 2 }
+  fill_in('ride_departure', with: @matching_ride.departure)
+  fill_in('ride_destination', with: @matching_ride.destination)
+  fill_in('ride_start_date', with: @matching_ride.start_date)
+  fill_in('ride_start_time', with: @matching_ride.start_time)
+  fill_in('ride_end_time', with: @matching_ride.end_time)
+  fill_in('ride_seats', with: @matching_ride.seats)
+end
+
 
 When("I fill in the form wrongly") do
   @ride = FactoryBot.build_stubbed(:ride)
@@ -44,7 +68,11 @@ When("I press Submit") do
 end
 
 Then("I should be on the auto-matching page") do
-  expect(page).to have_content("match")
+  expect(page).to have_content("Auto-Matched Rides")
+end
+
+Then("I should see no match") do
+  expect(page).to have_content("Sorry")
 end
 
 When ("I press follow the link View all rides") do
