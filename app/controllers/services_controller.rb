65cdd7881d7ca2d5f86219174f117f21b49249fe
David Ratliff <ratlidav@tamu.edu>
2019-04-14 22:19:12 +0000

    removed old login system

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,8 +1,8 @@
 class ServicesController < ApplicationController
   before_action :login_required, :only => [:index]
   def index
-     if current_user.isAdmin == true
-	   redirect_to admin_index_path
-	 end
+    if current_user.isAdmin == true
+      redirect_to admin_index_path
+    end
   end
 end
